37a38bf5ca683a0119346a54648aabaa5df84d26
lallyk <114976253+lallyk@users.noreply.github.com>
2022-10-06 14:54:13 +0530

    final commit demo1.js

--- demo1.js
+++ demo1.js
@@ -1,9 +1,4 @@
-//console.dir(document);
-//console.log(document.domain);
-//console.log(document.url);
-//document.title='firstdemo';
-//console.log(document.all);
-//document.all[0].textcontent='Items';
+
 var items=document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[2]);
