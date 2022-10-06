bed286fa120c60b3daeec333cf1c9f412175ab4a
lallyk <alally371@gmail.com>
2022-10-06 15:28:34 +0530

    classname and tagname

--- demo1.js
+++ demo1.js
@@ -9,5 +9,8 @@ console.log(items);
 console.log(items[2]);
 items[2].style.backgroundColor='green';
 document.getElementById("items").style.fontWeight = "bold";
-
+var it=document.querySelector('li');
+it.style.backgroundColor='blue';
+var it1=document.querySelector('new');
+it1.value='hello';
 
