0404d067dedff32da71b6727244561ef17b57b6e
lallyk <alally371@gmail.com>
2022-10-06 15:38:32 +0530

    classname and tagname

--- demo1.js
+++ demo1.js
@@ -1,9 +1,4 @@
-//console.dir(document);
-//console.log(document.domain);
-//console.log(document.url);
-//document.title='firstdemo';
-//console.log(document.all);
-//document.all[0].textcontent='Items';
+
 var items=document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[2]);
@@ -11,6 +6,6 @@ items[2].style.backgroundColor='green';
 document.getElementById("items").style.fontWeight = "bold";
 var it=document.querySelector('li');
 it.style.backgroundColor='blue';
-var it1=document.querySelector('new');
-it1.value='hello';
+var it1=document.querySelector('newid');
+it1.style.backgroundColor='blue';
 
