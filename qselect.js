9811a39c8f7c88336a1103da7fd4690662637c99
lallyk <114976253+lallyk@users.noreply.github.com>
2022-10-06 22:07:41 +0530

    queryselector file

---
+++ qselect.js
@@ -0,0 +1,6 @@
+var it=document.querySelector('li');
+it.style.backgroundColor='blue';
+var it1=document.querySelector(".new-item");
+it1.style.backgroundColor="red";
+it1.style.fontWeight='bold';
+
