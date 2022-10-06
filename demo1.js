f5a800304fc12d4d651d70bffc7f70148e46201b
lallyk <alally371@gmail.com>
2022-10-06 14:51:04 +0530

    adding demo.js

--- demo1.js
+++ demo1.js
@@ -1,1 +1,13 @@
-console.log('My first github program');
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.url);
+//document.title='firstdemo';
+//console.log(document.all);
+//document.all[0].textcontent='Items';
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[2]);
+items[2].style.backgroundColor='green';
+document.getElementById("items").style.fontWeight = "bold";
+
+
