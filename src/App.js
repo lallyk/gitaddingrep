80462e0a931dfb36efe3d9c364602339ccc9fbfd
lallyk <114976253+lallyk@users.noreply.github.com>
2022-11-16 15:30:46 +0530

    restaurant task3 display items

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <MealsSummary />
+    </Fragment>
+  );
+}
+
+export default App;
