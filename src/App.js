7c8d709c62c0328818a1ce400e50fb0b80d8c75b
lallyk <114976253+lallyk@users.noreply.github.com>
2022-12-01 01:05:00 +0530

    ecommerce task 7 loading

--- src/App.js
+++ src/App.js
@@ -5,29 +5,33 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [Loading, setLoading] = useState(false);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/people/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.height,
-            name: movieData.name,
-            year: movieData.birth_year,
-            eye: movieData.eye_color,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  async function fetchMoviesHandler() {
+    setLoading(true);
+    const response = await fetch("https://swapi.dev/api/people/");
+    const data = await response.json();
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.height,
+        name: movieData.name,
+        year: movieData.birth_year,
+        eye: movieData.eye_color,
+      };
+    });
+    setMovies(transformedMovies);
+    setLoading(false);
   }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!Loading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!Loading && movies.length === 0 && <p>Found no movies.</p>}
+        {Loading && <p>Loading...</p>}
       </section>
       <section>
         <MoviesList movies={movies} />
