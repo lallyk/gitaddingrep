fe1a5ef5d18108f0a779a6e77d5cfe9b63b41634
lallyk <114976253+lallyk@users.noreply.github.com>
2022-12-16 11:30:18 +0530

    expense tracker task10 edit and delete

--- src/components/Pages/ExpensesData.js
+++ src/components/Pages/ExpensesData.js
@@ -1,6 +1,7 @@
 import React from "react";
 //import AuthContext from "../Store/AuthContext";
 import classes from "./ExpenseData.module.css";
+import axios from "axios";
 
 const ExpenseData = (props) => {
   console.log(props.id);
@@ -9,14 +10,42 @@ const ExpenseData = (props) => {
   //const deleteHandler=()=>{
   //dispatch(expenseActions.deleteExpenses(props.id))
   //}
+  const deleteHandler = () => {
+    return async (dispatch) => {
+      const getData = async () => {
+        const response = await axios.delete(
+          "https://expensetracker-main-default-rtdb.firebaseio.com/expenses/-NJOOyRfmU8xoZBtd8cf.json"
+        );
+        const data = await response.data;
+        console.log(data);
+        return data;
+      };
+    };
+  };
+  const editHandler = () => {
+    return async (dispatch) => {
+      const getData = async () => {
+        const response = await axios.put(
+          "https://expensetracker-main-default-rtdb.firebaseio.com/expenses/-NJOOyRfmU8xoZBtd8cf.json"
+        );
+        const data = await response.data;
+        console.log(data);
+        return data;
+      };
+    };
+  };
 
   return (
     <div className={classes.expense}>
       <h3>{props.amount}</h3>
       <h3>{props.description}</h3>
       <h3>{props.category}</h3>
-      <button style={{ color: "black" }}>Edit</button>
-      <button style={{ color: "red" }}>Delete</button>
+      <button style={{ color: "black" }} onClick={deleteHandler}>
+        Delete
+      </button>
+      <button style={{ color: "red" }} onClick={editHandler}>
+        Edit
+      </button>
     </div>
   );
 };
