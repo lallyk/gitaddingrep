70db7ed2468a1c6c3a7dac512786453ef3cfbde4
lallyk <114976253+lallyk@users.noreply.github.com>
2023-03-16 02:23:52 +0530

    LoginPage Assignment

--- src/App.js
+++ src/App.js
@@ -1,53 +1,11 @@
-import AuthForm from "./Component/AuthForm/AuthForm";
-import TextEditing from "./Component/TextEditing/TextEditing";
-import InboxPage from "./Component/InboxPage.js/InboxPage";
-import { Route, Routes, Navigate } from "react-router-dom";
-import SentMessage from "./Component/InboxPage.js/Sentmessage/SentMessage";
-import { useEffect } from "react";
-import InboxList from "./Component/InboxPage.js/InboxList";
-import { useSelector, useDispatch } from "react-redux";
-import { UpdateMySentItem } from "./Store/Mail-thunk";
-import { useNavigate } from "react-router-dom";
+
+import './App.css';
+import LoginForm from './LoginForm';
 
 function App() {
-  let loginlocalstore = localStorage.getItem("islogin") === "true";
-  // console.log(loginlocalstore);
-  const navi = useNavigate();
-  const islogin = useSelector((state) => state.auth.islogin);
-  const Dispatch = useDispatch();
-  useEffect(() => {
-    if (loginlocalstore || islogin) {
-      navi("/main/text-edit");
-      console.log(" navi");
-    }
-    if (!loginlocalstore) {
-      navi("/login");
-    }
-  }, [loginlocalstore]);
-
-  const sentItem = useSelector((state) => state.mymail.sentItem);
-  const sendcount = useSelector((state) => state.mymail.sendcount);
-  useEffect(() => {
-    Dispatch(UpdateMySentItem(sentItem));
-  }, [sendcount]);
-  // console.log("app", sentItem);
   return (
     <div>
-      <Routes>
-        <Route path="/login" element={<AuthForm />}></Route>
-        {loginlocalstore && (
-          <Route path="/main/*" element={<InboxPage />}>
-            <Route path="inboxlist" element={<InboxList />} />
-            <Route path="text-edit" element={<TextEditing />} />
-            <Route path="sentmessage" element={<SentMessage />} />
-          </Route>
-        )}
-        {!loginlocalstore && (
-          <Route element={<Navigate replace to="login" />} />
-        )}
-
-        {/* <TextEditing></TextEditing> */}
-      </Routes>
+      <LoginForm />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import ReactDOM from "react-dom/client";
-import Store from "./Store/store";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Provider store={Store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
